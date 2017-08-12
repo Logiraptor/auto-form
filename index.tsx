ac1309ca22497b5fb6c5c30b325ba2cc325520a9
Patrick Oyarzun <patrickoyarzun@gmail.com>
2017-08-11 23:40:15 -0400

    Working code coverage

--- index.tsx
+++ index.tsx
@@ -1,267 +1,145 @@
-import * as React from "react";
-import { ReactHTML } from "react";
+import * as React from 'react';
+import { ReactHTML } from 'react';
 
-interface Subject {
-    name: string;
+type ComponentOrTag = keyof ElementTagNameMap | React.ComponentType;
+
+export interface FormProps {
+	element?: ComponentOrTag;
+	value: any;
+	onChange(x: any);
 }
 
-interface MyFormState {
-    subject: Subject;
-    other: {
-        "coolInnerStruct": {
-            dob: string;
-        };
-        age: string;
-        "FavoriteFood": string;
-    };
+export class AutoForm extends React.Component<FormProps, { value: any }> {
+	static defaultProps = {
+		element: 'form'
+	};
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: props.value
+		};
+	}
+
+	updateState(path: string[], value: any) {
+		const newState = this.setValueByPath(path, this.state.value, value);
+		this.props.onChange(newState);
+		this.setState({ value: newState });
+	}
+
+	setValueByPath(path, obj, val) {
+		if (path.length === 0) {
+			return val;
+		}
+		const key = path[0];
+
+		if (!(key in obj)) {
+			obj[key] = {};
+		}
+
+		const newSlot = this.setValueByPath(path.slice(1), obj[key], val);
+		obj[key] = newSlot;
+		return obj;
+	}
+
+	render() {
+		const FormComponent = this.props.element as React.ComponentType;
+		return (
+			<FormComponent>
+				{annotate((path, value) => this.updateState(path, value), this.state.value, this.props.children)}
+			</FormComponent>
+		);
+	}
 }
 
-// export class ExampleAutoWiredForm extends React.Component<{}, MyFormState> {
-//     state: MyFormState = {
-//         subject: {
-//             name: "",
-//         },
-//         other: {
-//             age: "",
-//             coolInnerStruct: {
-//                 dob: "",
-//             },
-//             FavoriteFood: "",
-//         },
-//     };
-
-//     render() {
-//         return (
-//             <Container>
-//                 <h4>CurrentValue:</h4>
-//                 <pre>{JSON.stringify(this.state, null, 2)}</pre>
-
-//                 <AutoWiredForm
-//                     value={this.state}
-//                     onChange={(value) => {
-//                         this.setState(value);
-//                     }}
-//                 >
-//                     <FormStruct name="subject">
-//                         <AutoInput name="name" type="text" />
-//                     </FormStruct>
-//                     <span>
-//                         <ul>
-//                             <li>Unrelated 1</li>
-//                         </ul>
-//                     </span>
-//                     <FormStruct name="other">
-//                         <FormStruct name="coolInnerStruct">
-//                             <AutoInput name="dob" type="date" />
-//                         </FormStruct>
-
-//                         <AutoInput name="age" type="number" />
-//                         <span>
-//                             <ul>
-//                                 <li>Unrelated 2</li>
-//                             </ul>
-//                         </span>
-//                         <AutoInput name="FavoriteFood" type="text" />
-//                     </FormStruct>
-
-//                     <FormStruct name="subject">
-//                         <AutoInput name="name" type="text" />
-//                     </FormStruct>
-//                 </AutoWiredForm>
-//             </Container>
-//         );
-//     }
-// }
-
-interface StructSchema {
-    fields: Field[];
-}
-
-interface Field {
-    name: string;
-    type: Type;
-}
-
-type Type = StructSchema | string;
-
-interface FormProps {
-    element?: React.ComponentType;
-    value: any;
-    onChange(x: any);
-}
-
-export class AutoForm extends React.Component<FormProps, { schema: StructSchema; value: any }> {
-    static defaultProps = {
-        element: "form",
-    };
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: props.value,
-            schema: { fields: [] },
-        };
-    }
-
-    componentDidMount() {
-        const fields = getSchema(this.props.children);
-        this.setState({ schema: { fields } });
-    }
-
-    updateState(path: string[], value: any) {
-        const newState = this.setValueByPath(path, this.state.value, value);
-        this.props.onChange(newState);
-        this.setState({ value: newState });
-    }
-
-    setValueByPath(path, obj, val) {
-        if (path.length === 0) {
-            return val;
-        }
-        const key = path[0];
-
-        if (!(key in obj)) {
-            obj[key] = {};
-        }
-
-        const newSlot = this.setValueByPath(path.slice(1), obj[key], val);
-        obj[key] = newSlot;
-        return obj;
-    }
-
-    render() {
-        const FormComponent = this.props.element as React.ComponentType;
-        return (
-            <FormComponent>
-                {annotate((path, value) => this.updateState(path, value), this.state.value, this.props.children)}
-            </FormComponent>
-        );
-    }
-}
-
-export function AutoWired<K extends keyof HTMLElementTagNameMap>(
-    type: K,
+export function AutoWired<K extends keyof ElementTagNameMap>(
+	type: K
 ): React.ComponentType<React.HTMLProps<ElementTagNameMap[K]> & { autowired?: boolean }>;
 export function AutoWired<P>(Component: React.ComponentType<P>): React.ComponentType<P & { autowired?: boolean }> {
-    const Inner = class extends React.Component {
-        render() {
-            const { autowire, ...rest } = this.props as any;
-            return <Component {...rest} />;
-        }
-    } as any;
+	const Inner = class extends React.Component {
+		render() {
+			const { autowire, ...rest } = this.props as any;
+			return <Component {...rest} />;
+		}
+	} as any;
 
-    if (Component.defaultProps) {
-        Inner.defaultProps = {
-            autowire: true,
-            ...Component.defaultProps as any,
-        };
-    } else {
-        Inner.defaultProps = { autowire: true };
-    }
+	if (Component.defaultProps) {
+		Inner.defaultProps = {
+			autowire: true,
+			...Component.defaultProps as any
+		};
+	} else {
+		Inner.defaultProps = { autowire: true };
+	}
 
-    return Inner;
+	return Inner;
 }
 
-export class FormStruct extends React.Component<{ name: string }> {
-    render() {
-        return <section>{this.props.children}</section>;
-    }
+export class FormStruct extends React.Component<{ element?: ComponentOrTag; name: string }> {
+	static defaultProps = {
+		element: 'section'
+	};
+
+	render() {
+		let Wrapper = this.props.element as React.ComponentType;
+		return <Wrapper>{this.props.children}</Wrapper>;
+	}
 }
 
 // Annotate adds value and onChange props to relevant elements
 // in children
 function annotate(onChange, value, element) {
-    function inner(prefix, innerValue, innerElement) {
-        function cloneWithAddedProps(clonedElement, clonedValue, clonedPrefix, props) {
-            if (React.Children.count(clonedElement.props.children) === 0) {
-                return React.cloneElement(clonedElement, { ...clonedElement.props, ...props });
-            } else {
-                return React.cloneElement(
-                    clonedElement,
-                    { ...clonedElement.props, ...props },
-                    inner(clonedPrefix, clonedValue, clonedElement.props.children),
-                );
-            }
-        }
+	function inner(prefix, innerValue, innerElement) {
+		function cloneWithAddedProps(clonedElement, clonedValue, clonedPrefix, props) {
+			if (React.Children.count(clonedElement.props.children) === 0) {
+				return React.cloneElement(clonedElement, { ...clonedElement.props, ...props });
+			} else {
+				return React.cloneElement(
+					clonedElement,
+					{ ...clonedElement.props, ...props },
+					inner(clonedPrefix, clonedValue, clonedElement.props.children)
+				);
+			}
+		}
 
-        // Element can be: an array:
-        if (React.Children.count(innerElement) > 1) {
-            return React.Children.map(innerElement, (child: any) => {
-                return inner(prefix, innerValue, child);
-            });
-        }
+		// Element can be: an array:
+		if (React.Children.count(innerElement) > 1) {
+			return React.Children.map(innerElement, (child: any) => {
+				return inner(prefix, innerValue, child);
+			});
+		}
 
-        // An element:
-        if (innerElement.props !== undefined) {
-            let newProps = {};
+		// An element:
+		if (innerElement.props !== undefined) {
+			let newProps = {};
 
-            if (innerElement.props.value !== undefined && innerElement.props.name !== undefined) {
-                newProps = {
-                    onChange(event) {
-                        onChange(prefix.concat(innerElement.props.name), event.target.value);
-                    },
-                };
-            }
+			if (innerElement.props.autowire) {
+				let value = '';
+				if (innerValue === undefined) {
+					innerValue = {};
+				}
+				if (innerElement.props.name in innerValue) {
+					value = innerValue[innerElement.props.name];
+				}
+				newProps = {
+					onChange(event) {
+						onChange(prefix.concat(innerElement.props.name), event.target.value);
+					},
+					value
+				};
+			}
 
-            if (innerElement.props.autowire) {
-                newProps = {
-                    onChange(event) {
-                        onChange(prefix.concat(innerElement.props.name), event.target.value);
-                    },
-                    value: innerValue[innerElement.props.name],
-                };
-            }
+			if (innerElement.type === FormStruct) {
+				prefix = prefix.concat([ innerElement.props.name ]);
+				innerValue = innerValue[innerElement.props.name];
+			}
 
-            if (innerElement.type === FormStruct) {
-                prefix = prefix.concat([ innerElement.props.name ]);
-                innerValue = innerValue[innerElement.props.name];
-            }
+			return cloneWithAddedProps(innerElement, innerValue, prefix, newProps);
+		}
 
-            return cloneWithAddedProps(innerElement, innerValue, prefix, newProps);
-        }
+		// Something else:
+		return innerElement;
+	}
 
-        // Something else:
-        return innerElement;
-    }
-
-    return inner([], value, element);
-}
-
-// getSchema discovers the schema of a form from its
-// children
-function getSchema(element): Field[] {
-    function collectFields(children) {
-        let fields: Field[] = [];
-
-        React.Children.forEach(children, (child: any) => {
-            if (typeof child.props === "undefined") {
-                return;
-            } else {
-                fields = fields.concat(getSchema(child));
-                return;
-            }
-        });
-
-        return fields;
-    }
-
-    // if (element.type === AutoInput) {
-    //     return [
-    //         {
-    //             name: element.props.name,
-    //             type: element.props.type,
-    //         },
-    //     ];
-    // }
-
-    if (React.Children.count(element) > 1) {
-        return collectFields(element);
-    }
-
-    if (element.type === FormStruct) {
-        const fields: Field[] = collectFields(element.props.children);
-        return [ { name: element.props.name, type: { fields } } ];
-    }
-
-    return collectFields(element.props.children);
+	return inner([], value, element);
 }
--- test/AutoForm.spec.tsx
+++ test/AutoForm.spec.tsx
@@ -1,117 +1,163 @@
-import { AutoForm, AutoWired, FormStruct } from "../";
-import "mocha";
-import { mount } from "enzyme";
-import * as React from "react";
-import { expect, use } from "chai";
-import * as chaiEnzyme from "chai-enzyme";
+import { AutoForm, AutoWired, FormStruct } from '../';
+import 'mocha';
+import { mount } from 'enzyme';
+import * as React from 'react';
+import { expect, use } from 'chai';
+import * as chaiEnzyme from 'chai-enzyme';
 
 use(chaiEnzyme());
 
 before(function() {
-    this.jsdom = require("jsdom-global")();
+	this.jsdom = require('jsdom-global')();
 });
 
 after(function() {
-    this.jsdom();
+	this.jsdom();
 });
 
-const AutoInput = AutoWired("input");
+const AutoInput = AutoWired('input');
 
 function clone(obj) {
-    // No sense adding another dependency just to clone literal objects
-    return JSON.parse(JSON.stringify(obj));
+	// No sense adding another dependency just to clone literal objects
+	return JSON.parse(JSON.stringify(obj));
 }
 
-describe("AutoForm", () => {
-    it("Binds values passed in to relevant inputs", () => {
-        let value = {
-            field_name: "Foobar",
-        };
+describe('AutoForm', () => {
+	it('Binds values passed in to relevant inputs', () => {
+		let value = {
+			field_name: 'Foobar'
+		};
 
-        let component = mount(
-            <AutoForm value={value} onChange={() => {}}>
-                <AutoInput name="field_name" type="text" />
-            </AutoForm>,
-        );
+		let component = mount(
+			<AutoForm value={value} onChange={() => {}}>
+				<AutoInput name="field_name" type="text" />
+			</AutoForm>
+		);
 
-        expect(component.find("input")).to.have.prop("value", value.field_name);
-    });
+		expect(component.find('input')).to.have.prop('value', value.field_name);
+	});
 
-    it("Binds complex values passed in to relevant inputs", () => {
-        let value = {
-            inner_struct: {
-                one_field: "Bob",
-                two_field: 5,
-            },
-            outer_field: "12/15/2016",
-        };
+	it('Binds complex values passed in to relevant inputs', () => {
+		let value = {
+			inner_struct: {
+				one_field: 'Bob',
+				two_field: 5
+			},
+			outer_field: '12/15/2016'
+		};
 
-        let component = mount(
-            <AutoForm value={value} onChange={() => {}}>
-                <FormStruct name="inner_struct">
-                    <AutoInput name="one_field" type="text" />
-                    <AutoInput name="two_field" type="text" />
-                </FormStruct>
-                <AutoInput name="outer_field" type="text" />
-            </AutoForm>,
-        );
+		let component = mount(
+			<AutoForm value={value} onChange={() => {}}>
+				<FormStruct name="inner_struct">
+					<AutoInput name="one_field" type="text" />
+					Just some text
+					<AutoInput name="two_field" type="text" />
+				</FormStruct>
+				<AutoInput name="outer_field" type="text" />
+			</AutoForm>
+		);
 
-        expect(component.find('input[name="one_field"]')).to.have.prop("value", value.inner_struct.one_field);
-        expect(component.find('input[name="two_field"]')).to.have.prop("value", value.inner_struct.two_field);
-        expect(component.find('input[name="outer_field"]')).to.have.prop("value", value.outer_field);
-    });
+		expect(component.find('input[name="one_field"]')).to.have.prop('value', value.inner_struct.one_field);
+		expect(component.find('input[name="two_field"]')).to.have.prop('value', value.inner_struct.two_field);
+		expect(component.find('input[name="outer_field"]')).to.have.prop('value', value.outer_field);
+	});
 
-    it("Triggers on change if any input changes", () => {
-        let value = {
-            inner_struct: {
-                one_field: "Bob",
-                two_field: 5,
-            },
-            outer_field: "12/15/2016",
-        };
+	it('Triggers on change if any input changes', () => {
+		let value = {
+			inner_struct: {
+				one_field: 'Bob',
+				two_field: 5
+			},
+			outer_field: '12/15/2016'
+		};
 
-        let lastValue = {};
+		let lastValue = {};
 
-        let component = mount(
-            <AutoForm
-                value={value}
-                onChange={(value) => {
-                    lastValue = value;
-                }}
-            >
-                <FormStruct name="inner_struct">
-                    <AutoInput name="one_field" type="text" />
-                    <AutoInput name="two_field" type="text" />
-                </FormStruct>
-                <AutoInput name="outer_field" type="text" />
-            </AutoForm>,
-        );
+		let component = mount(
+			<AutoForm
+				value={value}
+				onChange={(value) => {
+					lastValue = value;
+				}}
+			>
+				<FormStruct name="inner_struct">
+					<AutoInput name="one_field" type="text" />
+					<AutoInput name="two_field" type="text" />
+				</FormStruct>
+				<AutoInput name="outer_field" type="text" />
+				<FormStruct name="surprise_value">
+					<AutoInput name="undefined_in_value" type="text" />
+				</FormStruct>
+			</AutoForm>
+		);
 
-        const inputSelectors = [ 'input[name="one_field"]', 'input[name="two_field"]', 'input[name="outer_field"]' ];
+		const inputSelectors = [ 'one_field', 'two_field', 'outer_field', 'undefined_in_value' ];
 
-        inputSelectors.forEach((selector, i) => {
-            const preChangeValue = clone(lastValue);
-            let input = component.find(selector);
-            input.simulate("change", { target: { value: `New Value ${i}` } });
-            expect(lastValue).not.to.deep.eq(preChangeValue);
-        });
+		inputSelectors.forEach((selector, i) => {
+			const preChangeValue = clone(lastValue);
+			let input = component.find(`input[name="${selector}"]`);
+			input.simulate('change', { target: { value: `New Value ${i}` } });
+			expect(lastValue).not.to.deep.eq(preChangeValue);
+		});
 
-        expect(lastValue).to.deep.eq({
-            outer_field: "New Value 2",
-            inner_struct: {
-                one_field: "New Value 0",
-                two_field: "New Value 1",
-            },
-        });
-    });
+		expect(lastValue).to.deep.eq({
+			outer_field: 'New Value 2',
+			inner_struct: {
+				one_field: 'New Value 0',
+				two_field: 'New Value 1'
+			},
+			surprise_value: {
+				undefined_in_value: 'New Value 3'
+			}
+		});
+	});
 
-    it("Allows overriding the generated node");
+	it('Allows overriding the generated node', () => {
+		let component = mount(
+			<AutoForm element={'section'} value={{}} onChange={() => {}}>
+				<span id="inner" />
+			</AutoForm>
+		);
+
+		expect(component.find('section')).to.exist;
+	});
 });
 
-describe("AutoWired", () => {
-    it("Passes along all props");
+describe('AutoWired', () => {
+	it('Passes along all props to implicit components', () => {
+		let AutoComponent = AutoWired('input');
+		let component = mount(<AutoComponent name="Bobby" />);
+		expect(component).to.have.prop('name', 'Bobby');
+	});
+
+	it('Passes along all props to compound components', () => {
+		class CustomInput extends React.Component<{ name: string }> {
+			static defaultProps = {
+				name: 'foo'
+			};
+			render() {
+				return <input type="text" name={this.props.name} />;
+			}
+		}
+
+		let AutoComponent = AutoWired(CustomInput as any);
+		let component = mount(<AutoComponent name="Bobby" />);
+		expect(component).to.have.prop('name', 'Bobby');
+		expect(AutoComponent.defaultProps).to.deep.eq({
+			name: 'foo',
+			autowire: true
+		});
+	});
 });
 
-describe("FormStruct", () => {
-    it("Allows overriding the generated node");
+describe('FormStruct', () => {
+	it('Allows overriding the generated node', () => {
+		let component = mount(
+			<FormStruct element={'section'} name="test``">
+				<span id="inner" />
+			</FormStruct>
+		);
+
+		expect(component.find('section')).to.exist;
+	});
 });
